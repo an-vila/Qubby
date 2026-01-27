2b139ee2ecd8e07894539e9891b221b94fd854c8
Ander Vilariño <andervlrn@gmail.com>
2026-01-27 14:38:05 +0100

    Validaciones del form registro y correciones

--- client/src/app/features/auth/pages/register-page/register-page.component.ts
+++ client/src/app/features/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { UiInputComponent } from '../../../../shared/ui/ui-input/ui-input.component';
 import { UiButtonComponent } from '../../../../shared/ui/ui-button/ui-button.component';
@@ -8,28 +15,35 @@ import { UiButtonComponent } from '../../../../shared/ui/ui-button/ui-button.com
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    RouterModule,
-    UiInputComponent,
-    UiButtonComponent
-  ],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, UiInputComponent, UiButtonComponent],
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.css']
+  styleUrls: ['./register-page.component.css'],
 })
 export class RegisterPageComponent {
+  private fb = inject(FormBuilder);
   registerForm: FormGroup;
   showPassword = false;
   showConfirmPassword = false;
 
-  constructor(private fb: FormBuilder) {
-    this.registerForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required]
-    });
+  constructor() {
+    this.registerForm = this.fb.group(
+      {
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validators: this.passwordMatchValidator,
+      },
+    );
+  }
+
+  passwordMatchValidator(group: AbstractControl): ValidationErrors | null {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? null : { passwordsMismatch: true };
   }
 
   togglePassword() {
@@ -40,17 +54,33 @@ export class RegisterPageComponent {
     this.showConfirmPassword = !this.showConfirmPassword;
   }
 
+  errorMessage: string = '';
   onSubmit() {
-    if (this.registerForm.invalid) return;
-
+    // Validaciones de varios tipos sobre los input
     const { password, confirmPassword } = this.registerForm.value;
     if (password !== confirmPassword) {
-      alert("Las contraseñas no coinciden");
+      this.errorMessage = 'Las contraseñas no coinciden.';
       return;
     }
 
-    console.log(this.registerForm.value);
-    // aquí irá la llamada al backend
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      
+      if (this.registerForm.get('email')?.hasError('email')) {
+        this.errorMessage = 'El formato del correo electronico no es válido';
+        return;
+      }
+      
+      if (this.registerForm.get('password')?.hasError('minlength')) {
+        this.errorMessage = 'La contraseña debe tener al menos 6 caracteres';
+        return;
+      }
+
+      this.errorMessage = 'Todos los campos son obligatorios.';
+      return;
+    }
+
+    this.errorMessage = '';
+    console.log('Enviando...', this.registerForm.value);
   }
 }
-
