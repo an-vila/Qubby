baddc8e26e3645f6671660c40ff7130f0f4916e8
Ander Vilariño <andervlrn@gmail.com>
2026-02-26 22:56:19 +0100

    Modelo de caja creado

--- server/boxes/models.py
+++ server/boxes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Box(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='box'
+    )
+    name = models.CharField(max_length=100)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} (Usuario: {self.user})"
