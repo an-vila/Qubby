942a746ac34bdb9ddacbf2667fad5fc50dfbed5a
MATEO PAREDES <mateo_2012@hotmail.es>
2026-04-01 17:50:30 +0200

    pequeño cambio en el routes.ts

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-// 1. Importamos el Guard (asegúrate de que la ruta coincida con tu carpeta)
 import { authGuard } from './features/auth/guards/auth.guard';
 
 export const routes: Routes = [
-  // --- RUTA PÚBLICA (Login, Registro, etc.) ---
   {
     path: 'auth',
     loadChildren: () =>
@@ -11,19 +9,15 @@ export const routes: Routes = [
         .then(m => m.authRoutes)
   },
 
-  // --- RUTA PROTEGIDA (Home / Dashboard) ---
-  // Aquí es donde añadimos la magia del Guard 🛡️
+
   {
     path: 'home',
-    // Carga perezosa (Lazy loading) del componente Home
     loadComponent: () => 
   import('./features/home/pages/home-page/home-page.component')
         .then(m => m.HomePageComponent),
-    // Si no tienes token, el authGuard te bloqueará y te enviará al login
     canActivate: [authGuard]
   },
 
-  // --- RUTA PROTEGIDA (Detalle de caja → ver items) ---
   {
     path: 'home/box/:id',
     loadComponent: () =>
@@ -31,14 +25,14 @@ export const routes: Routes = [
         .then(m => m.BoxDetailPageComponent),
     canActivate: [authGuard]
   },
-  // --- REDIRECCIÓN POR DEFECTO ---
+
   {
     path: '',
     redirectTo: 'auth/login',
     pathMatch: 'full'
   },
   
-  // (Opcional) Ruta comodín por si escriben algo raro (404)
+
   {
     path: '**',
     redirectTo: 'auth/login'
