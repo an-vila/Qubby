d276548bab1e0f3c774eb7216289bdafa77f44cd
Ander Vilariño <andervlrn@gmail.com>
2026-05-04 15:08:25 +0200

    Update Interceptor

--- client/src/app/core/interceptors/auth-interceptor.ts
+++ client/src/app/core/interceptors/auth-interceptor.ts
@@ -1,11 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('access_token');
+  let token = localStorage.getItem('access_token');
 
-  if (token) {
+  if (!token || token === 'null' || token === 'undefined') {
+    token = sessionStorage.getItem('access_token');
+  }
+
+  if (token && token !== 'null' && token !== 'undefined') {
     const authReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`)
+      headers: req.headers.set('Authorization', `Bearer ${token}`),
     });
     return next(authReq);
   }
