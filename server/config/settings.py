6f679966d5a0807a7699e2d5e36460ef1428f606
Ander Vilariño <andervlrn@gmail.com>
2026-05-04 11:47:32 +0200

    Añadido Logging en settings.py

--- server/config/settings.py
+++ server/config/settings.py
@@ -177,3 +177,24 @@ DEFAULT_FROM_EMAIL = "noreply@qubby.com"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": True,
+        },
+    },
+}
